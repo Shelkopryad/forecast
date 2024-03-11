9a4c8f9c0fe455b3f0c1e16d84db9c75f9ac56e8
Shelkopryad <welkopr9d@gmail.com>
2024-03-11 08:06:09 +0100

    expenses chart

--- app/src/main/java/com/example/incomecalculator/HistoryOfMonthFragment.kt
+++ app/src/main/java/com/example/incomecalculator/HistoryOfMonthFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
@@ -15,6 +17,7 @@ import com.example.incomecalculator.data.DatabaseRepository
 import com.example.incomecalculator.databinding.HistoryOfMonthBinding
 import com.example.incomecalculator.view_models.DailyExpenseViewModel
 import kotlinx.coroutines.launch
+import java.math.BigDecimal
 
 class HistoryOfMonthFragment : Fragment() {
 
@@ -44,6 +47,45 @@ class HistoryOfMonthFragment : Fragment() {
                     if (financialMonth != null) {
                         val dailyExpenses =
                             dailyExpenseViewModel.loadDailyExpenses(financialMonth.id)
+
+                        val expensesByCategory = mutableMapOf<String, BigDecimal>()
+
+                        dailyExpenses.forEach {
+                            if (expensesByCategory[it.category] == null) {
+                                expensesByCategory[it.category] = it.amount
+                            } else {
+                                expensesByCategory[it.category] = expensesByCategory[it.category]!!.plus(it.amount)
+                            }
+
+                        }
+
+                        println(expensesByCategory.toString())
+
+                        val layout = binding.statistic
+
+                        expensesByCategory.onEachIndexed { index, entry ->
+                            val textView = TextView(context)
+                            textView.id = index
+                            textView.text = "${entry.key.uppercase()}: ${entry.value}"
+//                             вот тут надо добавить параметры
+// Параметры для TextView
+                            val params = ConstraintLayout.LayoutParams(
+                                ConstraintLayout.LayoutParams.WRAP_CONTENT,
+                                ConstraintLayout.LayoutParams.WRAP_CONTENT
+                            )
+
+                            params.leftToLeft = ConstraintLayout.LayoutParams.PARENT_ID
+                            params.topToBottom = ConstraintLayout.LayoutParams.PARENT_ID
+                            params.leftMargin = 16
+                            params.topMargin = 16
+
+                            textView.layoutParams = params
+
+
+                            layout.addView(textView)
+                        }
+
+
                         binding.expensesOfFinancialMonthsRecyclerView.adapter =
                             DailyExpenseAdapter(dailyExpenses)
                     }
